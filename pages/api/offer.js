557884e5154168f25a6c64c330396cc902d86f71
Lukas Gabsi <lukas.gabsi@towa.at>
2021-06-23 20:32:11 +0200

    allow all methods

--- pages/api/offer.js
+++ pages/api/offer.js
@@ -1,9 +1,5 @@
 import offer from '../../data/offer.json'
 
 export default function handler(req, res) {
-  if (req.method === 'POST') {
-    res.status(200).json(offer);
-  } else {
-    res.status(405).end();
-  }
+  res.status(200).json(offer);
 }
