673a5588ffcc53030ef30f9bf341fd1c11e65b1e
Lukas Gabsi <lukas.gabsi@towa.at>
2021-06-23 20:35:49 +0200

    cors middleware

--- pages/api/offer.js
+++ pages/api/offer.js
@@ -1,5 +1,18 @@
+import Cors from 'cors'
 import offer from '../../data/offer.json'
+import initMiddleware from '../../lib/init-middleware'
 
-export default function handler(req, res) {
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
+export default async function handler(req, res) {
+  await cors(req,res);
+
   res.status(200).json(offer);
 }
